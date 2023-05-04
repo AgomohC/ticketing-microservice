42dc43650e1a80262e4d9b851ebcb129b8125be4
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-05-04 05:24:20 +0100

    auth: added bad request error

---
+++ auth/src/errors/bad-request-error.ts
@@ -0,0 +1,18 @@
+import { CustomError } from "./CustomError"
+
+export class BadRequestError extends CustomError {
+	statusCode = 401
+
+	constructor(public message: string) {
+		super(message)
+
+		Object.setPrototypeOf(this, BadRequestError.prototype)
+	}
+	serializeErrors() {
+		return [
+			{
+				message: this.message,
+			},
+		]
+	}
+}
