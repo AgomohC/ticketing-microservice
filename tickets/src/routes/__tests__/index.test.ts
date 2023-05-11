e78ac7491073f4f401649463d9404083648e66cc
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-05-11 05:08:09 +0100

    added index route and tests

---
+++ tickets/src/routes/__tests__/index.test.ts
@@ -0,0 +1,16 @@
+import supertest from "supertest"
+import app from "../../app"
+const createTicket = async () => {
+	return supertest(app)
+		.post("/api/tickets")
+		.set("Cookie", global.signin())
+		.send({ title: "kdmjidm", price: 20 })
+}
+it("can fetch a list of tickets", async () => {
+	await createTicket()
+	await createTicket()
+	await createTicket()
+	const response = await supertest(app).get("/api/tickets").send().expect(200)
+
+	expect(response.body.length).toEqual(3)
+})
