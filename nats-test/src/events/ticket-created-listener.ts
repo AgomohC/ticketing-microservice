bf1fbe75b6705fb6a8505e5dbfdbb559ecb2be85
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-05-14 19:18:24 +0100

    added ticket created listener to nats test

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,13 @@
+import { type Message, type Stan } from "node-nats-streaming"
+import { Listener } from "./base-listener"
+import { Subjects } from "./subjects"
+import { TicketCreatedEvent } from "./subjects"
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+	subject: Subjects.TicketCreated = Subjects.TicketCreated
+	queueGroupName = "payments-service"
+	onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+		console.log("Event data!", data)
+		msg.ack()
+	}
+}
