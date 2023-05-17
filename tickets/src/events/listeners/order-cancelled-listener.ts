1073ac446bc4fb35ad4a2ff4afff6e77e0402cb1
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-05-17 15:41:40 +0100

    added order cancelled listener tests

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,27 @@
+import { Listener, OrderCancelledEvent, Subjects } from "@femtoace/common"
+import { queueGroupName } from "./queue-group-name"
+import { Message } from "node-nats-streaming"
+import { Ticket } from "../../models/ticket"
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher"
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+	subject: Subjects.OrderCancelled = Subjects.OrderCancelled
+	queueGroupName = queueGroupName
+	async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+		const ticket = await Ticket.findById(data.ticket.id)
+		if (!ticket) {
+			throw new Error("Ticket not found")
+		}
+		ticket.set({ orderId: undefined })
+		await ticket.save()
+		await new TicketUpdatedPublisher(this.client).publish({
+			title: ticket.title,
+			id: ticket.id,
+			userId: ticket.userId,
+			price: ticket.price,
+			version: ticket.version,
+			orderId: ticket.orderId,
+		})
+
+		msg.ack()
+	}
+}
